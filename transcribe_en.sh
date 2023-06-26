cbe8e9dacf6e8cd5951b421a131351f5df37a243
Denis Volk <the.denis.volk@gmail.com>
2023-06-26 16:22:56 +0400

    Update UX to batch process multiple files in one command

---
+++ transcribe_en.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+wav_dir="audio/wav"
+
+find "${wav_dir}" -type f -iname "*.wav" | while read -r input_file; do
+    echo "Transcribing ${input_file}..."
+    ./whisper.cpp/main -m whisper.cpp/models/ggml-large.bin -f "$input_file" -nt
+    echo "Transcribing done."
+done
