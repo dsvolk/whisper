c996bf8092f62fc3a99e72cacbda34b0180cbc75
Denis Volk <the.denis.volk@gmail.com>
2023-07-01 13:43:32 +0400

    Update transcribe* commands to save output to text file

--- transcribe_auto.sh
+++ transcribe_auto.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 wav_dir="audio/wav"
+output_dir="transcriptions"
 
 find "${wav_dir}" -type f -iname "*.wav" | while read -r input_file; do
     echo "Transcribing ${input_file}..."
-    ./whisper.cpp/main -m whisper.cpp/models/ggml-large.bin -f "$input_file" -nt -l auto
+    ./whisper.cpp/main -m whisper.cpp/models/ggml-large.bin -f "$input_file" -nt -l auto | tee "${output_dir}/$(basename "${input_file%.*}").txt"
     echo "Transcribing done."
 done
--- transcribe_en.sh
+++ transcribe_en.sh
@@ -4,6 +4,6 @@ wav_dir="audio/wav"
 
 find "${wav_dir}" -type f -iname "*.wav" | while read -r input_file; do
     echo "Transcribing ${input_file}..."
-    ./whisper.cpp/main -m whisper.cpp/models/ggml-large.bin -f "$input_file" -nt
+    ./whisper.cpp/main -m whisper.cpp/models/ggml-large.bin -f "$input_file" -nt | tee "${output_dir}/$(basename "${input_file%.*}").txt"
     echo "Transcribing done."
 done
