d246a31d328ff5ba79fc4734a9864bdc911c09c1
Denis Volk <the.denis.volk@gmail.com>
2023-10-25 12:55:31 +0300

    Fix convert.sh

--- convert.sh
+++ convert.sh
@@ -3,9 +3,10 @@
 input_dir="audio/input"
 output_dir="audio/wav"
 
-find "${input_dir}" -type f -iname "*.*" -not -name ".DS_Store" | while read -r input_file; do
-    echo "Input file: $input_file"
-    output_file="${output_dir}/$(basename "${input_file%.*}")_converted.wav"
+find "${input_dir}" -type f -iname "*.*" -not -name ".DS_Store" | while read -r file; do
+    base_input_file="${file##*/}"
+    input_file="${input_dir}/${base_input_file}"
+    output_file="${output_dir}/${base_input_file}.wav"
     echo "Converting ${input_file} to ${output_file}..."
     ffmpeg -y -hide_banner -loglevel error -i "${input_file}" -acodec pcm_s16le -ar 16000 "${output_file}"
     echo "Done."
