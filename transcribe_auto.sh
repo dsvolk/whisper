e8ed1fa081eb3bf04229c0f4ffd7c51b282cce22
Denis Volk <the.denis.volk@gmail.com>
2023-11-09 05:19:17 +0400

    Switch to Whisper-Large-V2

--- transcribe_auto.sh
+++ transcribe_auto.sh
@@ -5,6 +5,6 @@ output_dir="transcriptions"
 
 find "${wav_dir}" -type f -iname "*.wav" | while read -r input_file; do
     echo "Transcribing ${input_file}..."
-    ./whisper.cpp/main -m whisper.cpp/models/ggml-large.bin -f "$input_file" -nt -l auto | tee "${output_dir}/$(basename "${input_file%.*}").txt"
+    ./whisper.cpp/main -m whisper.cpp/models/ggml-large-v2.bin -f "$input_file" -nt -l auto | tee "${output_dir}/$(basename "${input_file%.*}").txt"
     echo "Transcribing done."
 done
