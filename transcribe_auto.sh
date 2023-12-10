9507afe621360bb41656428040d2dcc97528d9e3
Denis Volk <the.denis.volk@gmail.com>
2023-12-10 14:06:17 +0400

    Hopefully, fix transcribe_auto.sh to handle complex filenames

--- transcribe_auto.sh
+++ transcribe_auto.sh
@@ -3,8 +3,10 @@
 wav_dir="audio/wav"
 output_dir="transcriptions"
 
-find "${wav_dir}" -type f -iname "*.wav" | while read -r input_file; do
+find "${wav_dir}" -type f -iname "*.wav" -exec bash -c '
+    input_file="$1"
+    output_file="${2}/$(basename "${input_file%.*}").txt"
     echo "Transcribing ${input_file}..."
-    ./whisper.cpp/main -m whisper.cpp/models/ggml-large-v2.bin -f "$input_file" -nt -l auto | tee "${output_dir}/$(basename "${input_file%.*}").txt"
+    ./whisper.cpp/main -m whisper.cpp/models/ggml-large-v3.bin -f "${input_file}" -nt -l auto | tee "${output_file}"
     echo "Transcribing done."
-done
+' _ {} "${output_dir}" \;
